f8d84d0e855d037311e0654af4c99957bd1aec66
Ugo Pellissier de Féligonde <ugo.defeli@gmail.com>
2020-10-12 22:38:56 +0200

    remplissage panier : choix lentille, vérification de l'existence du produit OK

--- js/cart.js
+++ js/cart.js
@@ -8,35 +8,27 @@ cart = JSON.parse(cart); // traduit au format js
 const apiUrl = "http://localhost:3000";
 fetch(`${apiUrl}/api/cameras`).then(response=>response.json())
 
-.then( (cameras) => {
-    totalCost = 0;
+    .then( (cameras) => {
+        totalCost = 0;
+        let quantity = 1;
 
-    for (product of cart) {
-
-        let productId = Object.keys(cart);
-        let product = Object.entries(cart);
-        let quantity = product[0][1].quantity;
-        let lense = product[0][1].lense;
-
-        // verfication de l'existence du produit en quantité > 1
-        if (!isNaN(quantity) && quantity > 0) {
-
-        // generation dynamique de la liste d'article du panier
-        const block = document.createElement("div");
-        block.className ="row";
-        block.innerHTML =
-        `
-        <p class="col-2"><a href="product.html?id=${camera._id}">${camera.name}</a></p>
-        <p class="col-4">${lense}</p>
-        <p class="col-2">${camera.price / 100} €</p>
-        <p class="col-2">${quantity}</p>
-        <p class="col-2">${quantity * camera.price / 100} €</p>
-        `
-        ;
-        cartList.appendChild(block);// injection du code
-        totalCost = totalCost + ( quantity * camera.price / 100)
-    }
-}
+        for (camera of cameras) {
+            // generation dynamique de la liste d'article du panier
+            const block = document.createElement("div");
+            block.className ="row";
+            block.innerHTML =
+            `
+            <p class="col-2"><a href="product.html?id=${camera._id}">${camera.name}</a></p>
+            <p class="col-4">${camera.lense}</p>
+            <p class="col-2">${camera.price / 100} €</p>
+            <p class="col-2">${quantity}</p>
+            <p class="col-2">${quantity * camera.price / 100} €</p>
+            `
+            ;
+            cartList.appendChild(block);// injection du code
+            totalCost = totalCost + ( quantity * camera.price / 100); // ajout
+        }
+    });
 
 // cout total
 let totalCostPlace = document.getElementById('total-cost'); // localisation du lieu d'injection
@@ -48,5 +40,3 @@ block.innerHTML =
 `
 ;
 totalCostPlace.appendChild(block);
-
-});
--- js/product.js
+++ js/product.js
@@ -58,22 +58,34 @@ fetch(`${apiUrl}/api/cameras/${id}`).then(response=>response.json())
             let cart = localStorage.getItem("cart"); // creation variable cart qui récupère ce qui se trouve déjà dans le panier
             cart = JSON.parse(cart); // traduit au format js
             
+            let quantity = counter
+            
             let product = { // on créé le produit qui récupère le infos de la page actuelle
                 "id" : camera._id,
-                "quantity" : counter,
-                "lense" : selectedLense // le [i] du tableau lenses
+                "lenses" : {
+                    [selectedLense] : Number(quantity),
+                 }
             };
 
             if (cart !== null) { // si le panier existe
-                console.log("le panier existe");
+                console.log("le panier existe déjà");
                 if(cart[product.id] != undefined) { // s'il le produit est déjà dans le panier
-                    console.log("le produit est déjà dans le panier")
-                    console.log(cart[product.id])
-                    product.quantity = product.quantity + counter;
+                    console.log("ce produit " + product.id + " est déjà dans le panier");
+                    if(cart[product.id].lenses[selectedLense] != undefined) { // si la lentille existe, on lui ajoute une quantité
+                        console.log("cette lentille existe dans le panier, on ajoute à la quantité déjà dedans (" + cart[product.id].lenses[selectedLense] + ") le compteur actuel (" + product.lenses[selectedLense] + ") qui passe donc à " + (cart[product.id].lenses[selectedLense] + product.lenses[selectedLense]));
+                        cart[product.id].lenses[selectedLense] = cart[product.id].lenses[selectedLense] + product.lenses[selectedLense]
+                    } else { // si la lentille n'existe pas, on augmente sa quantité
+                        console.log("cette lentille n'existait pas dans le panier, on la créée");
+                        cart[product.id].lenses[selectedLense] = product.lenses[selectedLense]; // QUESTION POUR CHRIS pourquoi cela n'écrase-t-il pas le précédent ?
+                    }
+                }
+                else { // si le produit n'est pas dans le panier, on l'ajoute
+                    console.log("le produit " + product.id + " n'était pas dans le panier");
+                    cart[product.id] = product;
                 }
             }
             else { // si le panier n'existe pas
-                console.log("le panier n'existe pas, on créé le produit");
+                console.log("le panier n'existait pas, on créé le produit");
                 cart = { // on créé le produit
                     [product.id]: product
                 }
